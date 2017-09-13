6c0fc60998f731c34eccf13ab19f23b63eea994e
Alexey <alex.agoshkov6@gmail.com>
2017-09-13 20:56:50 +0300

    Add files via upload

---
+++ rda/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace myradio
+{
+    partial class Radio
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Radio));
+            this.bt = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bt
+            // 
+            this.bt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bt.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.bt.Location = new System.Drawing.Point(80, 34);
+            this.bt.Name = "bt";
+            this.bt.Size = new System.Drawing.Size(75, 23);
+            this.bt.TabIndex = 0;
+            this.bt.Tag = "";
+            this.bt.Text = "Play";
+            this.bt.UseVisualStyleBackColor = true;
+            this.bt.Click += new System.EventHandler(this.bt_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(213, 35);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(18, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "˄";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(18, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "˅";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(203, 66);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(28, 17);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Off";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Channel -";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.AccessibleRole = System.Windows.Forms.AccessibleRole.Sound;
+            this.trackBar1.Location = new System.Drawing.Point(160, 90);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.trackBar1.Size = new System.Drawing.Size(75, 45);
+            this.trackBar1.TabIndex = 7;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.Value = 100;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(164, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Mute -";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(141, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(25, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "100";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(96, 93);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Volume -";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "No Station";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(149, 20);
+            this.textBox1.TabIndex = 14;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(167, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(37, 23);
+            this.button4.TabIndex = 15;
+            this.button4.Text = ">";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_2);
+            // 
+            // Radio
+            // 
+            this.AcceptButton = this.bt;
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.Sound;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(232, 114);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.bt);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Radio";
+            this.Text = "Radio";
+            this.Load += new System.EventHandler(this.Radio_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+     
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ rda/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using WMPLib;
+
+namespace myradio
+{
+    public partial class Radio : Form
+    {
+
+        public Radio()
+        {
+            InitializeComponent();
+
+            list.Add("http://relay.181.fm:8030/;?.mp3");
+            list.Add("http://64.150.176.87:8287/;?.mp3");
+            list.Add("http://relay.181.fm:8132");
+            list.Add("http://ep256.hostingradio.ru:8052/europaplus256.mp3");
+            list.Add("http://air2.radiorecord.ru:805/rr_320");
+            list.Add("http://air2.radiorecord.ru:805/deep_320");
+         
+        }
+
+        private string[] nameChannel = new string[] {"The Eagle", "Wild Rock", "Classic Rock Hits"
+                                                    ,"Europa Plus", "Radio Record", "Record Deep"};
+
+        WindowsMediaPlayer wmPlayer = new WindowsMediaPlayer();
+
+
+        private void bt_Click(object sender, EventArgs e)
+        {
+
+            PlayURL(list[count]);
+            CountChannel();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (count <= 0)
+            {
+                count = list.Count;
+            }
+            count--;
+            PlayURL(list.ElementAt(count));
+            CountChannel();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            count++;
+            if (count > list.Count - 1)
+            {
+                count = 0;
+            }
+            PlayURL(list.ElementAt(count));
+            CountChannel();
+        }
+
+        private void PlayURL(string URL)
+        {
+            wmPlayer.URL = URL;
+            wmPlayer.controls.play();
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            wmPlayer.controls.stop();
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!mut)
+            {
+                wmPlayer.settings.mute = true;
+                mut = true;
+                button3.Text = "On";
+
+            }
+            else
+            {
+                wmPlayer.settings.mute = false;
+                mut = false;
+                button3.Text = "Off";
+
+            }
+        }
+
+        private void trackBar1_Scroll_1(object sender, EventArgs e)
+        {
+            wmPlayer.settings.volume = trackBar1.Value;
+            label4.Text = "" + trackBar1.Value;
+        }
+
+        private void CountChannel()
+        {
+            for (int i = 0; i < nameChannel.Length; i++)
+            {
+               
+                if (nameChannel.Length > count)
+                {
+                    label1.Text = nameChannel[count];
+                }
+                else
+                {
+                    label1.Text = "" + count;
+                }
+            }
+
+            
+        }
+
+
+        #region Private
+
+        private List<string> list = new List<string>();
+        private int count = 0;
+        private bool mut = false;
+       
+       
+
+        #endregion
+        
+        #region 
+        private void Radio_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+        }
+    
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void rb_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rb_CheckedChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        #endregion
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button4_Click_2(object sender, EventArgs e)
+        {
+            PlayURL(textBox1.Text);
+            label1.Text = "I Don't Know";
+        }
+    }
+}
